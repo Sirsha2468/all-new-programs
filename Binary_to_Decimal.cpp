fc70123a02702ff35fed23d7efd71108930c3fe2
Sirsha Dey <69218962+Sirsha2468@users.noreply.github.com>
2022-10-12 01:34:52 +0530

    Update Binary_to_Decimal.cpp

--- Binary_to_Decimal.cpp
+++ Binary_to_Decimal.cpp
@@ -16,3 +16,31 @@ int main() {
 
   return 0;
 }
+
+Method 2 - using function
+/*Contributed by Sirsha*/
+#include<iostream>
+#include<cmath>
+#include<stdlib.h>
+using namespace std;
+int Binary_to_Decimal(long int);
+int main()
+{
+    long int n;
+    system("cls");
+    printf("Enter the binary number for coverting it to decimal: ");
+//	scanf("%d",&n);
+    cin >> n;
+    cout << n <<" in decimal: " << Binary_to_Decimal(n);
+}
+int Binary_to_Decimal(long int n)
+{
+    int dec = 0, i = 0, rem;
+
+    while (n!=0) {
+        rem = n % 10;
+        n /= 10;
+        dec += rem * pow(2, i);
+        ++i;
+    }
+}
